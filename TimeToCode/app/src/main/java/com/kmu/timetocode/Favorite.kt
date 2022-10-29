6227089a29b4f7ebb3754fdaf0c7159281fffb0e
이예원 <81704418+scarletYewon@users.noreply.github.com>
2022-10-29 21:58:49 +0900

    favorite

---
+++ TimeToCode/app/src/main/java/com/kmu/timetocode/Favorite.kt
@@ -0,0 +1,11 @@
+package com.kmu.timetocode
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Favorite : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_favorite)
+    }
+}
