155bd8762c8672b195b97d6f134e0b9b4f310ead
sehyeon518 <lifethis21@kookmin.ac.kr>
2022-10-31 20:48:14 +0900

    MyChallenge
    MuChallenge add Gridlayout

--- TimeToCode/app/src/main/java/com/kmu/timetocode/MainActivity.kt
+++ TimeToCode/app/src/main/java/com/kmu/timetocode/MainActivity.kt
@@ -2,11 +2,20 @@ package com.kmu.timetocode
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import android.content.Intent
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        val myChallenge = findViewById<View>(R.id.myChallenge) as Button
+        myChallenge.setOnClickListener {
+            Toast.makeText(applicationContext, "내 챌린지 인증", Toast.LENGTH_SHORT).show()
+            val intent = Intent(applicationContext, MychallengeActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
