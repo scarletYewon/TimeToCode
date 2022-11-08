b3632083f17f17f15ec910e170ea9241e3b4401f
이예원 <81704418+scarletYewon@users.noreply.github.com>
2022-11-09 02:25:19 +0900

    NoticePage ListView구현

---
+++ TimeToCode/app/src/main/java/com/kmu/timetocode/NoticeListModel.kt
@@ -0,0 +1,6 @@
+package com.kmu.timetocode
+
+data class NoticeListModel(
+    val image : String,
+    val content: String,
+)
--- TimeToCode/app/src/main/java/com/kmu/timetocode/NoticePage.kt
+++ TimeToCode/app/src/main/java/com/kmu/timetocode/NoticePage.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ListView
 
 class NoticePage : Fragment() {
     override fun onCreateView(
@@ -12,7 +13,23 @@ class NoticePage : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ) : View? {
-        val rootView = inflater.inflate(R.layout.fragment_notice_page, container, false)
+        val rootView : View = inflater.inflate(R.layout.fragment_notice_page, container, false)
+//        val mActivity = activity as MainActivity
+//        val btn_change = rootView.findViewById(R.id.button_change);
+//        btn_change.setOnClickListener{
+//            activity.changeFragment(2)
+//        }
+
+        val list_array = arrayListOf<NoticeListModel>(
+            NoticeListModel("a","챌린지 이름"),
+            NoticeListModel("a","챌린지 이름"),
+            NoticeListModel("a","챌린지 이름"),
+            NoticeListModel("a","챌린지 이름"),
+            NoticeListModel("a","챌린지 이름"),
+
+        )
+        var Adapter = NoticeListAdapter(requireContext(),list_array)
+        rootView?.findViewById<ListView>(R.id.listview_notice_fragment)?.adapter = Adapter
 
         return rootView
     }
