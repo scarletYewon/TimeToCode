ce1a09ab31e19936f436d26f25499e4ba4ae0eae
sehyeon518 <lifethis21@kookmin.ac.kr>
2022-11-09 16:31:41 +0900

    Edit fragment kotlin files'
    certification fragment : edit drawable src
    main fragment: AppCompatActivity -> Fragment

--- TimeToCode/app/src/main/java/com/kmu/timetocode/MainFragment.kt
+++ TimeToCode/app/src/main/java/com/kmu/timetocode/MainFragment.kt
@@ -2,6 +2,9 @@ package com.kmu.timetocode
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
 import android.widget.CalendarView
 import android.widget.TextView
@@ -9,15 +12,20 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 
-class MainFragment : AppCompatActivity() {
+class MainFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_main)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ) : View? {
+        val rootView = inflater.inflate(R.layout.fragment_main, container, false)
 
-        val userName = findViewById<TextView>(R.id.userName)
-        val userLevel = findViewById<TextView>(R.id.userLevel)
-        val myChallenge = findViewById<Button>(R.id.myChallenge)
-        val calendarView = findViewById<CalendarView>(R.id.calenderView)
+        val userName = rootView?.findViewById<TextView>(R.id.userName)
+        val userLevel = rootView?.findViewById<TextView>(R.id.userLevel)
+        val myChallenge = rootView?.findViewById<TextView>(R.id.myChallenge)
+        val calendarView = rootView?.findViewById<TextView>(R.id.calenderView)
+
+        return rootView
     }
 }
