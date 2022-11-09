6943ffdb5a911f8608f642db8ee35c983e014b7d
이예원 <81704418+scarletYewon@users.noreply.github.com>
2022-11-09 15:12:12 +0900

    nav바
    상단바에 하트버튼, 종버튼 추가
    하트 -> FavoriteFragment이동
    종 -> NoticeFragment이동

--- TimeToCode/app/src/main/java/com/kmu/timetocode/FavoritePage.kt
+++ TimeToCode/app/src/main/java/com/kmu/timetocode/FavoritePage.kt
@@ -5,10 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.ListView
-import androidx.fragment.app.FragmentManager
-import androidx.navigation.findNavController
 
 class FavoritePage : Fragment() {
     override fun onCreateView(
