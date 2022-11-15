c83ce7b576472a9b23750d7d6022f7d1a896fbb4
AnCha <tory912@gmail.com>
2022-11-15 19:03:12 +0900

    챌린지 등록의 두 번째, 세 번째 화면 수정
    챌린지 등록
    두 번째 화면
    - 이미지 선택 라디오 버튼 그룹 수정 완료
    세 번째 화면
    - 하루 인증 횟수 선택에 따라서 조건(상세 횟수, 인증 간격) VISIBLE, GONE 구현
    - timePickerDialog -> mode:spinner 해결 완료, header 구현 필요

--- TimeToCode/app/src/main/java/com/kmu/timetocode/FragmentAddChallenge3.kt
+++ TimeToCode/app/src/main/java/com/kmu/timetocode/FragmentAddChallenge3.kt
@@ -1,14 +1,15 @@
 package com.kmu.timetocode
 
+import android.app.TimePickerDialog
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import com.kmu.timetocode.databinding.FragmentAddChallenge2Binding
 import com.kmu.timetocode.databinding.FragmentAddChallenge3Binding
+import java.util.*
 
 class FragmentAddChallenge3 : Fragment() {
 
@@ -23,12 +24,44 @@ class FragmentAddChallenge3 : Fragment() {
     ): View? {
 
         _binding = FragmentAddChallenge3Binding.inflate(inflater, container, false)
+        binding.selectCount.visibility = View.GONE
+        binding.interval.visibility = View.GONE
 
         binding.rGroupCount.setOnCheckedChangeListener{radioGroup, i->
             when(i){
-                binding.rBtnCountOption1.id -> binding.selectCount.visibility = View.GONE
-                binding.rBtnCountOption2.id -> binding.selectCount.visibility = View.VISIBLE
+                binding.rBtnCountOption1.id -> {
+                    binding.selectCount.visibility = View.GONE
+                    binding.interval.visibility = View.GONE
+                }
+                binding.rBtnCountOption2.id -> {
+                    binding.selectCount.visibility = View.VISIBLE
+                    binding.interval.visibility = View.VISIBLE
+                }
             }
+        }
+
+        binding.interval.setOnClickListener{
+            var listener = TimePickerDialog.OnTimeSetListener { timePicker, i, i2 ->
+                binding.editInterval.text = "${i}시간 ${i2}분"
+            }
+            var picker = TimePickerDialog(requireContext(),listener, 0, 0, true)
+            picker.show()
+        }
+
+        binding.startTime.setOnClickListener{
+            var listener = TimePickerDialog.OnTimeSetListener { timePicker, i, i2 ->
+                binding.editStartTime.text = "${i}시 ${i2}분"
+            }
+            var picker = TimePickerDialog(requireContext(),listener, 0, 0, true)
+            picker.show()
+        }
+
+        binding.endTime.setOnClickListener{
+            var listener = TimePickerDialog.OnTimeSetListener { timePicker, i, i2 ->
+                binding.editEndTime.text = "${i}시 ${i2}분"
+            }
+            var picker = TimePickerDialog(requireContext(), listener, 23, 59, true)
+            picker.show()
         }
 
         binding.btnGoAdd4.setOnClickListener{
