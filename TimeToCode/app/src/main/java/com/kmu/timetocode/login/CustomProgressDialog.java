93fd608c106039f420b47be1205f11556a427d87
hongbuly <hongbuly@naver.com>
2022-11-17 16:03:12 +0900

    Modify Login
    다이얼로그 디자인 변경.
    로그인 관련 패키지 분할.

---
+++ TimeToCode/app/src/main/java/com/kmu/timetocode/login/CustomProgressDialog.java
@@ -0,0 +1,15 @@
+package com.kmu.timetocode.login;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.view.Window;
+
+import com.kmu.timetocode.R;
+
+public class CustomProgressDialog extends Dialog {
+    public CustomProgressDialog(Context context) {
+        super(context);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.custom_dialog);
+    }
+}
