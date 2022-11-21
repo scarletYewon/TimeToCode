b2dc7813b40a17165772ab44dcc975b90feb5d31
이예원 <81704418+scarletYewon@users.noreply.github.com>
2022-11-21 16:13:58 +0900

    파일경로이동

--- TimeToCode/app/src/main/java/com/kmu/timetocode/certicenter/Certifbox.kt
+++ TimeToCode/app/src/main/java/com/kmu/timetocode/certicenter/Certifbox.kt
@@ -1,10 +1,11 @@
-package com.kmu.timetocode
+package com.kmu.timetocode.certicenter
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.kmu.timetocode.R
 
 class Certifbox : Fragment() {
     override fun onCreateView(
