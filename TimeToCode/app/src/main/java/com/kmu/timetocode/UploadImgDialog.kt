9214b291a28b57a6469f88f3330ec0dfe8f76370
AnCha <tory912@gmail.com>
2022-11-23 01:14:01 +0900

    챌린지 등록 이미지 가져오기 기능 구현
    챌린지 등록 두번째 네번째 화면 이미지 가져오기 기능 구현
    - dialogType에 따라서 앨범 선택, 사진 촬영 버튼 유무 달라짐
    - 이미지 선택 및 사진 촬영에 따라 가져온 이미지를 화면에 미리 보여주기 구현

--- TimeToCode/app/src/main/java/com/kmu/timetocode/UploadImgDialog.kt
+++ TimeToCode/app/src/main/java/com/kmu/timetocode/UploadImgDialog.kt
@@ -1,37 +1,41 @@
 package com.kmu.timetocode
 
+import android.app.Activity
 import android.app.Activity.RESULT_OK
 import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.Color
+import android.graphics.ImageDecoder
 import android.graphics.drawable.ColorDrawable
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.os.Environment
 import android.provider.MediaStore
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.activity.result.ActivityResultLauncher
+import android.widget.Toast
 import androidx.core.content.FileProvider
+import androidx.core.os.bundleOf
 import androidx.fragment.app.DialogFragment
-import com.kmu.timetocode.databinding.FragmentAddChallenge2Binding
 import com.kmu.timetocode.databinding.TakeImgDialogLayoutBinding
+import java.io.ByteArrayOutputStream
 import java.io.File
 import java.io.IOException
 import java.text.SimpleDateFormat
 import java.util.*
 
 
-class UploadImgDialog(val title: String) : DialogFragment() {
+class UploadImgDialog(val title: String, val dialogType: Int) : DialogFragment() {
 
     private var _binding: TakeImgDialogLayoutBinding? = null
 
     private val binding get() = _binding!!
-    private lateinit var resultLauncher : ActivityResultLauncher<Intent>
 
-    val REQUEST_IMAGE_CAPTURE = 1  //카메라 사진 촬영 요청 코드 *임의로 값 입력
-    lateinit var currentPhotoPath : String //문자열 형태의 사진 경로값 (초기값을 null로 시작하고 싶을 때 - lateinti var)
+    val REQUEST_IMAGE_CAPTURE = 1  //카메라 사진 촬영 요청 코드
+    lateinit var currentPhotoPath : String //문자열 형태의 사진 경로값
     val REQUEST_IMAGE_PICK = 10
 
     override fun onCreateView(
@@ -44,70 +48,149 @@ class UploadImgDialog(val title: String) : DialogFragment() {
 
         binding.uploadImgTitle.text = title
 
+        when(dialogType) {
+            1-> binding.selectCam.visibility = View.GONE
+        }
+
+
+        return binding.root
+    }
+
+    override fun onResume() {
+        super.onResume()
         binding.selectAlbum.setOnClickListener{
             pickImg()
-            dismiss()
         }
 
         binding.selectCam.setOnClickListener{
             takeImg()
-            dismiss()
+
         }
 
         binding.selectCancel.setOnClickListener{
             dismiss()
         }
-
-        return binding.root
     }
 
     private fun takeImg() {
         Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
-            takePictureIntent.resolveActivity(requireContext().packageManager)?.also {
-                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+            Log.i("test","실행1")
+
+            if (takePictureIntent.resolveActivity(requireContext().packageManager) != null) {
+                // 촬영한 사진 파일로 만들기
+                val photoFile: File? =
+                    try {
+                        createImageFile()
+                    } catch (ex: IOException) {
+                        Log.d("TAG", "사진파일 생성 중 에러 발생")
+                        null
+                    }
+
+                // 그림파일을 성공적으로 만들었다면 onActivityForResult로 보내기
+                photoFile?.also {
+                    Log.i("also","여기는 also")
+                    val photoURI: Uri = FileProvider.getUriForFile(
+                        requireContext(), "com.kmu.timetocode.fileprovider", it
+                    )
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                    Log.i("also","여기는 also2")
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+                    Log.i("also","여기는 also3")
+
+                }
             }
         }
+        }
 
-    }
 
     private fun createImageFile(): File? {
         val timestamp : String  = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date()) // 이미지 파일 이름
         val storeageDir : File? = requireContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES)  // 스트리지 디렉토리 경로
+        Log.i("test","여기는 create")
 
         return File.createTempFile("JPEG_${timestamp}_", ".jpg", storeageDir)
             .apply { currentPhotoPath = absolutePath}
-    }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-//        val resolver = requireActivity().contentResolver
-//
-//        if(requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-//            val imageBitmap = data?.extras?.get("data") as Bitmap
-//            imageView.setImageBitmap(imageBitmap)
-//        }else if (requestCode == REQUEST_IMAGE_PICK && resultCode == RESULT_OK) {
-//            val currentImageUrl : Uri? = data?.data
-//            try{
-//                val bitmap = MediaStore.Images.Media.getBitmap(resolver, currentImageUrl)
-//                imageView.setImageBitmap(bitmap)
-//            }catch (e: Exception){
-//                e.printStackTrace()
-//            }
-//        }
     }
-
 
     private fun pickImg() {
-        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        val intent = Intent(Intent.ACTION_PICK)
         intent.setType("image/*")
         startActivityForResult(intent, REQUEST_IMAGE_PICK)
     }
 
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val resolver = requireActivity().contentResolver
+//        Log.i("test","실행??")
+//
+//
+//        if(requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+//            val imageString = data?.extras?.get("data") as String
+//            val bundle = bundleOf("bundleKey" to imageString)
+//            Log.v("test","실행2")
+//
+//            setFragmentResult("requestKey", bundle)
+//
+//        }else if (requestCode == REQUEST_IMAGE_PICK && resultCode == RESULT_OK) {
+//            val currentImageUrl : Uri? = data?.data
+//            try{
+//                val bitmap = MediaStore.Images.Media.getBitmap(resolver, currentImageUrl)
+////                imageView.setImageBitmap(bitmap)
+//            }catch (e: Exception){
+//                e.printStackTrace()
+//            }
+//        }
+        when (requestCode){
+            1 -> {
+                if(requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK){
+
+                    // 카메라로부터 받은 데이터가 있을경우에만
+                    val file = File(currentPhotoPath)
+                    if (Build.VERSION.SDK_INT < 28) {
+                        val bitmap = MediaStore.Images.Media
+                            .getBitmap(resolver, Uri.fromFile(file))  //Deprecated
+                        Log.i("file","여기는 getbitmap")
+                        Toast.makeText(activity, "여기는 getbitmap", Toast.LENGTH_SHORT).show()
+                        dismiss()
+                    }
+                    else{
+                        val decode = ImageDecoder.createSource(resolver,
+                            Uri.fromFile(file))
+                        val bitmap = ImageDecoder.decodeBitmap(decode)
+                        Log.i("file","여기는 getbitmap2")
+                        val sendData = bitmapToString(bitmap)
+                        val bundle = bundleOf("takeBundleKey" to sendData)
+                        requireActivity().supportFragmentManager.setFragmentResult("takeRequestKey", bundle)
+                        dismiss()
+                    }
+                }
+            }
+            10->{
+                if (resultCode == Activity.RESULT_OK && requestCode == REQUEST_IMAGE_PICK){
+                    val currentImg : Uri? = data?.data
+                    val imgData = currentImg.toString()
+                    Log.i("a","이것은 선택한 이미지: ${imgData}")
+                    val bundle = bundleOf("pickBundleKey" to imgData)
+                    requireActivity().supportFragmentManager.setFragmentResult("pickRequestKey", bundle)
+                    dismiss()
+
+                }
+            }
+        }
+    }
 
 
+    fun bitmapToString(bitmap: Bitmap): String {
+        val stream = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream)
 
+        val bytes = stream.toByteArray()
 
-        override fun onDestroyView() {
+        return Base64.getEncoder().encodeToString(bytes)
+    }
+
+    override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
--- TimeToCode/app/src/main/java/com/kmu/timetocode/add/FragmentAddChallenge4.kt
+++ TimeToCode/app/src/main/java/com/kmu/timetocode/add/FragmentAddChallenge4.kt
@@ -1,13 +1,21 @@
 package com.kmu.timetocode.add
 
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.normal.TedPermission
+import com.kmu.timetocode.UploadImgDialog
 import com.kmu.timetocode.databinding.FragmentAddChallenge3Binding
 import com.kmu.timetocode.databinding.FragmentAddChallenge4Binding
+import java.util.*
 
 
 class FragmentAddChallenge4 : Fragment() {
@@ -23,9 +31,54 @@ class FragmentAddChallenge4 : Fragment() {
 
         _binding = FragmentAddChallenge4Binding.inflate(inflater, container, false)
 
+        binding.btnSelectExplainImg.setOnClickListener {
+            requestPermission()
+        }
+
     // TODO: 챌린지 생성 후 어디로 가야하는지 결정
 
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        requireActivity().supportFragmentManager.setFragmentResultListener("takeRequestKey",this) { requestKey, bundle ->
+            bundle.getString("takeBundleKey")?.let {
+                val imageBytes = Base64.getDecoder().decode(it)
+                val image = BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.size)
+                binding.explainImgView.setImageBitmap(image)
+                Log.i("take","takeImg를 통해 fragment")
+            }
+        }
+
+        requireActivity().supportFragmentManager.setFragmentResultListener("pickRequestKey",this) { requestKey, bundle ->
+            bundle.getString("pickBundleKey")?.let {
+                val image = Uri.parse(it)
+                binding.explainImgView.setImageURI(image)
+                Log.i("take","pickImg를 통해 fragment")
+            }
+        }
+    }
+
+    private fun requestPermission() {
+        TedPermission.create()
+            .setPermissionListener(object : PermissionListener {
+
+                //권한이 허용됐을 때
+                override fun onPermissionGranted() {
+                    UploadImgDialog("인증방법 설명 이미지 가져오기",2).show(parentFragmentManager,"takeImgDialog")
+
+                }
+
+                //권한이 거부됐을 때
+                override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+                    Toast.makeText(activity, "카메라 기능 실행", Toast.LENGTH_SHORT).show()
+                }
+            })
+            .setDeniedMessage("카메라 권한을 허용해주세요.")// 권한이 없을 때 띄워주는 Dialog Message
+            .setPermissions(android.Manifest.permission.READ_EXTERNAL_STORAGE,android.Manifest.permission.WRITE_EXTERNAL_STORAGE,android.Manifest.permission.CAMERA)// 얻으려는 권한
+            .check()
     }
 
     override fun onDestroyView() {
