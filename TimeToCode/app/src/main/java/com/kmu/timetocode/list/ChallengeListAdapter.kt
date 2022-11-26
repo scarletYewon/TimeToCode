edc3ef9e1f6a48ec63da936e808bec632ae0dcb6
AnCha <tory912@gmail.com>
2022-11-26 17:28:51 +0900

    챌린지 리스트 서버연결 성공 및 화면에 보이지 않던 오류 해결
     E/RecyclerView: No adapter attached; skipping layout
    
    다음과 같은 오류 발생하였으나
    recyclerView의 setHasFixedSize(true) 를 정의하고 layoutManger, adapter를 재정의하여 해결함.

--- TimeToCode/app/src/main/java/com/kmu/timetocode/list/ChallengeListAdapter.kt
+++ TimeToCode/app/src/main/java/com/kmu/timetocode/list/ChallengeListAdapter.kt
@@ -1,22 +1,41 @@
 package com.kmu.timetocode.list
 
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import androidx.core.net.toUri
 import androidx.recyclerview.widget.RecyclerView
 import com.kmu.timetocode.databinding.ChallengeListItemBinding
 
-class ChallengeListAdapter(val context: Context, val challengeListArray: ArrayList<ChallengeListModel>): RecyclerView.Adapter<ChallengeListAdapter.ListViewHolder>() {
+class ChallengeListAdapter(val context: Context, private val challengeListArray: ArrayList<ChallengeListModel>): RecyclerView.Adapter<ChallengeListAdapter.ListViewHolder>() {
 
     class ListViewHolder(val binding: ChallengeListItemBinding) : RecyclerView.ViewHolder(binding.root) {
 //        TODO: img 추가
         fun setList(item: ChallengeListModel){
+
             binding.textChallegeNameInList.text = item.title
             binding.textMadeUserInList.text = item.owner
             binding.challengePrtcp.text = "+"+item.prtcp+"명"
             binding.challengeTag1.text = item.tag1
             binding.challengeTag2.text = item.tag2
 
+        val pos = adapterPosition
+        if(pos!= RecyclerView.NO_POSITION)
+        {
+            itemView.setOnClickListener {
+                listener?.onItemClick(itemView,item,pos)
+            }
+        }
+        }
+
+        interface OnItemClickListener{
+            fun onItemClick(v: View, data: ChallengeListModel, pos : Int)
+        }
+        private var listener : OnItemClickListener? = null
+        fun setOnItemClickListener(listener : OnItemClickListener) {
+            this.listener = listener
         }
     }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
@@ -30,6 +49,17 @@ class ChallengeListAdapter(val context: Context, val challengeListArray: ArrayLi
     override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
 
         holder.setList(challengeListArray.get(position))
+    }
+
+    override fun onViewDetachedFromWindow(holder: ListViewHolder) {
+        super.onViewDetachedFromWindow(holder)
+        Log.e("onViewDetachedFromWindow","${holder.adapterPosition}")
+    }
+
+    override fun onAttachedToRecyclerView(recyclerView: RecyclerView) {
+        super.onAttachedToRecyclerView(recyclerView)
+        Log.e("onAttachedToRecyclerView","onAttachedToRecyclerView")
+
     }
 
 }
--- TimeToCode/app/src/main/java/com/kmu/timetocode/list/FragmentChallengeList.kt
+++ TimeToCode/app/src/main/java/com/kmu/timetocode/list/FragmentChallengeList.kt
@@ -1,17 +1,31 @@
 package com.kmu.timetocode.list
 
+import android.R
 import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.VolleyError
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.kmu.timetocode.add.AddChallenge
 import com.kmu.timetocode.databinding.FragmentChallengeListBinding
+import com.kmu.timetocode.login.UserProfile
+import org.json.JSONArray
+
 
 class FragmentChallengeList : Fragment() {
 
+
+    var queue: RequestQueue?=null
+    var challengeListAdapter: ChallengeListAdapter? = null
+    var challengeListArray: ArrayList<ChallengeListModel> = ArrayList()
 
     private var _binding: FragmentChallengeListBinding? = null
 
@@ -33,18 +47,26 @@ class FragmentChallengeList : Fragment() {
             ChallengeItemModel("a","챌린지 이름","github"),
             ChallengeItemModel("a","챌린지 이름","github")
         )
-        val challengeListArray = arrayListOf<ChallengeListModel>(
-            ChallengeListModel("a","챌린지 이름","생성자","60","github", "알고리즘"),
-            ChallengeListModel("a","챌린지 이름","생성자","60","github", "알고리즘"),
-            ChallengeListModel("a","챌린지 이름","생성자","60","github", "알고리즘"),
-            ChallengeListModel("a","챌린지 이름","생성자","60","github", "알고리즘"),
-            ChallengeListModel("a","챌린지 이름","생성자","60","github", "알고리즘"),
-            ChallengeListModel("a","챌린지 이름","생성자","60","github", "알고리즘")
+//        val challengeListArray = arrayListOf<ChallengeListModel>(
+//            ChallengeListModel("a","챌린지 이름","생성자","60","github", "알고리즘"),
+//            ChallengeListModel("a","챌린지 이름","생성자","60","github", "알고리즘"),
+//            ChallengeListModel("a","챌린지 이름","생성자","60","github", "알고리즘"),
+//            ChallengeListModel("a","챌린지 이름","생성자","60","github", "알고리즘"),
+//            ChallengeListModel("a","챌린지 이름","생성자","60","github", "알고리즘"),
+//            ChallengeListModel("a","챌린지 이름","생성자","60","github", "알고리즘")
+//
+//        )
 
-        )
 
-        binding.listChallenge.layoutManager = LinearLayoutManager(requireContext())
-        binding.listChallenge.adapter = ChallengeListAdapter(requireContext(),challengeListArray)
+        binding.listChallenge.setHasFixedSize(true)
+
+        var manager = LinearLayoutManager(requireContext())
+        binding.listChallenge.layoutManager = manager
+
+        var chAdapter = ChallengeListAdapter(requireContext(),challengeListArray)
+
+        binding.listChallenge.adapter = chAdapter
+        showAllList()
 
         binding.listNew.layoutManager = LinearLayoutManager(requireContext(),LinearLayoutManager.HORIZONTAL,false)
         binding.listNew.adapter = ChallengeItemAdapter(requireContext(),challengeItemArray)
@@ -60,12 +82,59 @@ class FragmentChallengeList : Fragment() {
         super.onViewCreated(view, savedInstanceState)
     }
 
+    private fun showAllList() {
+        val myId = UserProfile.getId()
+        val url = "https://android-pkfbl.run.goorm.io/challenge/allChallengeDataList"
+        val sr = StringRequest(
+            Request.Method.GET, url,
+            { response: String? ->
+                challengeListArray =
+                    ArrayList<ChallengeListModel>()
+                try {
+                    val jsonArray = JSONArray(response)
+                    for (i in 0 until jsonArray.length()) {
+                        val jsonObject = jsonArray.getJSONObject(i)
+                        val nameTag = jsonObject.getString("nameChallenge")
+                        val imageLink = jsonObject.getString("imageLink")
+                        val madeName = jsonObject.getString("name")
+                        val count = jsonObject.getInt("countUser")
+                        val nameTagList = nameTag.split("%")
+                        challengeListArray.add(
+                            ChallengeListModel(
+                                imageLink,
+                                nameTagList[0],
+                                madeName,
+                                Integer.toString(count),
+                                nameTagList[1],
+                                nameTagList[2]
+                            )
+                        )
+                    }
+                } catch (e: Exception) {
+                    Log.e("SearchListJSON", response!!)
+                }
+                challengeListAdapter = ChallengeListAdapter(requireContext(), challengeListArray)
+                binding.listChallenge.adapter= challengeListAdapter
+            }
+        ) { error: VolleyError ->
+            Log.e(
+                "searchList",
+                "ee"
+            )
+        }
+        sr.setShouldCache(false)
+        queue = Volley.newRequestQueue(requireContext())
+        queue!!.add(sr)
+    }
+
 //    TODO: 찜 버튼 클릭 시 버튼 이미지 변경
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
+
 
 
 }
