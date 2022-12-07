75575ce9bab8c29d4567017afbead79359b70f4f
이예원 <81704418+scarletYewon@users.noreply.github.com>
2022-12-07 18:09:23 +0900

    사진적용

--- TimeToCode/app/src/main/java/com/kmu/timetocode/cdp/CdpListAdapter.kt
+++ TimeToCode/app/src/main/java/com/kmu/timetocode/cdp/CdpListAdapter.kt
@@ -1,12 +1,16 @@
 package com.kmu.timetocode.cdp
 
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.net.toUri
+import com.bumptech.glide.Glide
+import com.google.firebase.storage.FirebaseStorage
 import com.kmu.timetocode.R
 
 class CdpListAdapter(val context: Context, val list: ArrayList<CdpListModel>): BaseAdapter() {
@@ -27,16 +31,27 @@ class CdpListAdapter(val context: Context, val list: ArrayList<CdpListModel>): B
             holder.view_tag2 = view.findViewById(R.id.tag2)
 
             view.tag = holder
+            val challengeItem = list[position] // 챌린지 list 안의 챌린지 한개
+
+            val fullName = challengeItem.title
+            holder.view_title?.text = fullName?.split("%")?.get(0)
+            holder.view_owner?.text = challengeItem.owner
+            holder.view_count?.text = "+" + challengeItem.countUser.toString() + "명"
+            holder.view_tag1?.text = "#" + challengeItem.tag1
+            holder.view_tag2?.text = "#" + challengeItem.tag2
+            Log.d("test", "UserImages_" + fullName)
+
+            val storage: FirebaseStorage = FirebaseStorage.getInstance("gs://timetocode-13747.appspot.com/")
+            val fileExt = arrayOf(".jpeg", ".jpg", "")
+            for(i in fileExt)
+                storage.getReference().child("UserImages_" + fullName + i).downloadUrl
+                    .addOnSuccessListener { uri ->
+                        Glide.with(context).load(uri.toString().toUri()).into(holder.view_image!!)
+                    }
         } else {
             holder = convertview.tag as ViewHolder
             view = convertview
         }
-        val item = list[position]
-        holder.view_title?.text = item.title
-        holder.view_owner?.text = item.owner
-        holder.view_count?.text = "+" + item.countUser.toString() + "명"
-        holder.view_tag1?.text = "#" + item.tag1
-        holder.view_tag2?.text = "#" + item.tag2
 
         return view
 
