5e9d9e754b9f28b61857c6c2c82dadd1dd447c15
이예원 <81704418+scarletYewon@users.noreply.github.com>
2022-12-09 22:35:36 +0900

    record삽질

--- TimeToCode/app/src/main/java/com/kmu/timetocode/recordcenter/RecordFragment.kt
+++ TimeToCode/app/src/main/java/com/kmu/timetocode/recordcenter/RecordFragment.kt
@@ -1,6 +1,7 @@
 package com.kmu.timetocode.recordcenter
 
 import android.content.Context
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.view.*
@@ -31,7 +32,7 @@ class RecordFragment : Fragment() {
     var myList: GridView?=null
     var title: String?=null
     var now = LocalDate.now().format(DateTimeFormatter.ofPattern("MMdd"))
-    var challengeId : String?=null
+    var photoList: ArrayList<Uri>?= ArrayList()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -57,13 +58,8 @@ class RecordFragment : Fragment() {
         Log.d("test getMessage", "현재 챌린지 페이지 이름은 " + title.toString())
         challengeTitle?.text = title
 
-        getChallengeId()
 
-//        adapter?.addItem(Record(1, R.drawable.ttcwhite))
-//        adapter?.addItem(Record(2, R.drawable.ttcwhite))
-//        adapter?.addItem(Record(3, R.drawable.ttcwhite))
-//        adapter?.addItem(Record(4, R.drawable.ttcwhite))
-
+        adapter = photoList?.let { GridViewAdapter(requireContext(), it) }
         listView?.setAdapter(adapter)
         rootView?.findViewById<GridView>(R.id.recordList)?.adapter = adapter
 
@@ -73,13 +69,35 @@ class RecordFragment : Fragment() {
         val fileExt = arrayOf(".jpeg", ".jpg", "")
 
         for(i in fileExt)
-            storage.getReference().child("UserImages_" + fullName + i).downloadUrl
+            storage.getReference().child("UserImages_$fullName$i").downloadUrl
                 .addOnSuccessListener { uri ->
                     Glide.with(requireActivity().getApplicationContext()).load(uri).into(tmpChallenge!!)
                     Log.d("기록센터 사진 불러오기", uri.toString())
                 }
 
-        var recordList:ArrayList<Record>
+        val dd = "UserImages_" + UserProfile.getId().toString()+ "%" + title + "%" + now
+        Log.e("ddddddd",dd)
+//        val storage: FirebaseStorage = FirebaseStorage.getInstance("gs://timetocode-13747.appspot.com/")
+        storage.getReference().child(dd ).downloadUrl
+            .addOnSuccessListener { uri ->
+                Log.e("photo", uri.toString())
+                photoList?.add(uri)
+                Log.e("photo", photoList?.get(0).toString())
+            }
+//            uri ->
+//            Glide.with(requireActivity().getApplicationContext()).load(uri).into(holder.rc_image!!)
+//            Log.d("기록 사진 불러오기", uri.toString())
+//
+//        var a = 5
+//        for(i in 0..a){
+//            storage.getReference().child(photoList?.get(i).toString()).downloadUrl
+//                .addOnSuccessListener { uri ->
+//                    Glide.with(requireActivity().getApplicationContext()).load(uri).into(GridViewAdapter(context,photoList))
+//                    Log.d("dddddddddd", uri.toString())
+//                }
+//
+//        }
+
 
 
 
@@ -87,10 +105,9 @@ class RecordFragment : Fragment() {
     }
 
     /* 그리드뷰 어댑터 */
-    inner class GridViewAdapter(val context: Context, val list: ArrayList<Record>) : BaseAdapter() {
+    inner class GridViewAdapter(val context: Context, val list: ArrayList<Uri>) : BaseAdapter() {
         override fun getCount(): Int { return list.size }
 
-        fun addItem(item: Record) { list.add(item) }
         override fun getItem(position: Int): Any { return list[position] }
         override fun getItemId(position: Int): Long { return position.toLong() }
 
@@ -105,27 +122,12 @@ class RecordFragment : Fragment() {
                 holder.rc_title = view.findViewById(R.id.recordTitle)
                 holder.rc_image = view.findViewById(R.id.recordImage)
 
-
-                holder.rc_title?.text = list[position].chId.toString()
-                list[position].resId?.let {holder.rc_image?.setImageResource(it)}
-
                 view.tag = holder
             } else {
                 holder = convertview.tag as ViewHolder
                 view = convertview
             }
 
-            val recordItem = list[position]
-            val resourceId = context.resources.getIdentifier(recordItem.resId.toString(), "drawable", context.packageName)
-            holder.rc_image?.setImageResource(resourceId)
-            val storage: FirebaseStorage = FirebaseStorage.getInstance("gs://timetocode-13747.appspot.com/")
-            val fileExt = arrayOf(".jpeg", ".jpg", "")
-            for(i in fileExt)
-                storage.getReference().child("UserImages_" + UserProfile.getId().toString()+ "%" + title + "%" + now).downloadUrl
-                    .addOnSuccessListener { uri ->
-                        Glide.with(requireActivity().getApplicationContext()).load(uri).into(holder.rc_image!!)
-                        Log.d("기록 사진 불러오기", uri.toString())
-                        }
 
             return view //뷰 객체 반환
         }
@@ -133,86 +135,5 @@ class RecordFragment : Fragment() {
             var rc_title: TextView? = null
             var rc_image: ImageButton? = null
         }
-    }
-
-    private fun getChallengeId() {
-        val url = "https://android-pkfbl.run.goorm.io/challenge/nameChallenge?nameChallenge=" + title
-        val sr: StringRequest = object : StringRequest( Method.GET, url,
-            Response.Listener { response: String? ->
-                try {
-                    val jsonArray = JSONArray(response)
-                    val jsonObject = jsonArray.getJSONObject(0)
-                    val idChallenge = jsonObject.getInt("idChallenge")
-                    val nameChallenge = jsonObject.getString("nameChallenge")
-                    val introduce = jsonObject.getString("intruduce")
-                    val imageLink = jsonObject.getString("imageLink")
-                    val frequency = jsonObject.getInt("frequency")
-                    val possibleStartTime = jsonObject.getInt("possibleStartTime")
-                    val possibleEndTime = jsonObject.getInt("possibleEndTime")
-                    val count = jsonObject.getInt("count")
-                    val countInterval = jsonObject.getString("countInterval")
-                    val challengePostCount = jsonObject.getInt("challengePostCount")
-                    val madeIdUser = jsonObject.getInt("madeIdUser")
-                    val countUser = jsonObject.getInt("countUser")
-                    val certificationWay = jsonObject.get("certificationWay")
-                    val certificationWayImageLink = jsonObject.get("certificationWayImageLink")
-                    val endDate = jsonObject.getString("endDate")
-                    Log.d("test get challengeID", idChallenge.toString())
-                    showMyList(idChallenge.toString())
-                } catch (e: Exception) {
-                    Log.e("Challenge name to id", response!!)
-                }
-            },
-            Response.ErrorListener { error: VolleyError ->
-            }) {
-            @Throws(java.lang.Error::class)
-            override fun getParams(): MutableMap<String,String>? {
-                val params: MutableMap<String, String> = HashMap()
-//                params["idUser"] = myId.toString()
-//                Log.e(params.toString(),"params")
-                return params
-            }
-        }
-        sr.setShouldCache(false)
-        queue = Volley.newRequestQueue(requireContext())
-        queue!!.add(sr)
-    }
-
-    private fun showMyList(chID:String) {
-        val myId = UserProfile.getId()
-        val url = "https://android-pkfbl.run.goorm.io/challengePost/all?idUser=$myId&idChallenge=$chID"
-        val sr: StringRequest = object: StringRequest(Method.GET, url,
-        Response.Listener{response: String? ->
-            val recordList = ArrayList<Record>()
-            try {
-                val jsonArray = JSONArray(response)
-                for (i in 0 until jsonArray.length()) {
-                    val jsonObject = jsonArray.getJSONObject(i)
-                    Log.d("test record list in recordCenter", jsonObject.toString())
-                    val postPhoto = jsonObject.get("postPhoto")
-                    val idUser = jsonObject.get("idUser")
-                    val idChallenge = jsonObject.getInt("idChallenge")
-                    recordList.add(Record(idChallenge, R.drawable.ttcwhite))
-                    challengeId = idChallenge.toString()
-                }
-            } catch (e: Exception) {
-                Log.e("MyRecordJSON", response!!)
-            }
-            adapter = GridViewAdapter(requireContext(), recordList)
-            myList?.setAdapter(adapter)
-        },
-            Response.ErrorListener { error: VolleyError ->
-            }) {
-            @Throws(java.lang.Error::class)
-            override fun getParams(): MutableMap<String,String>? {
-                val params: MutableMap<String, String> = HashMap()
-//                params["idUser"] = myId.toString()
-//                Log.e(params.toString(),"params")
-                return params
-            }
-        }
-        sr.setShouldCache(false)
-        queue = Volley.newRequestQueue(requireContext())
-        queue!!.add(sr)
     }
 }
